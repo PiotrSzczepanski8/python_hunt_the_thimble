ec8b1e53f233c0205bb507baad03c1c513c6c449
PiotrSzczepanski8 <114732037+PiotrSzczepanski8@users.noreply.github.com>
2024-04-24 19:29:18 +0200

    prototype_version

---
+++ main.py
@@ -0,0 +1,116 @@
+from variables import *
+
+import pygame
+
+pygame.init()
+
+font = pygame.font.SysFont("Arial", 50)
+
+pygame.display.set_caption(game_name)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            steps += 1
+            if event.key == pygame.K_w:
+                player_y -= 1
+                if player_y < 1:
+                    print("Auć! uderzasz w ścianę!")
+                    player_y = 1
+            if event.key == pygame.K_s:
+                player_y += 1
+                if player_y > GAME_HEIGHT:
+                    print("Auć! uderzasz w ścianę!")
+                    player_y = GAME_HEIGHT
+            if event.key == pygame.K_a:
+                player_x -= 1
+                if player_x < 1:
+                    print("Auć! uderzasz w ścianę!")
+                    player_x = 1
+            if event.key == pygame.K_d:
+                player_x += 1
+                if player_x > GAME_WIDTH:
+                    print("Auć! uderzasz w ścianę!")
+                    player_x = GAME_WIDTH
+
+            distance_after_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
+
+            print('before', distance_before_move)
+            print('after', distance_after_move)
+
+            if distance_after_move == 0:
+                text = 'Klucz jest Twój, możesz iść otworzyć skarb'
+            elif distance_before_move > distance_after_move:
+                text = 'Ciepło'
+            elif distance_before_move < distance_after_move:
+                text = 'Zimno'
+
+            distance_before_move = distance_after_move
+
+            img = font.render(text, True, '#212121')
+        
+    screen.fill(background_color)
+    pygame.draw.rect(screen, '#212121', pygame.Rect(margin_left, margin_top, GAME_WIDTH*50, GAME_HEIGHT*50))
+    for i in range(GAME_WIDTH):
+        pygame.draw.line(screen, background_color, ((margin_left + i*50), margin_top), (margin_left + i*50, margin_top+GAME_HEIGHT*50), 3)
+    for i in range(GAME_WIDTH):
+        pygame.draw.line(screen, background_color, ((margin_left), margin_top + i*50), (margin_left+GAME_WIDTH*50, margin_top + i*50), 3)
+    pygame.draw.rect(screen, player_color, pygame.Rect(margin_left + player_x*50+2-50, margin_top+player_y*50+2-50, 47, 47))
+    if 'img' in globals():
+        screen.blit(img, (50, 200))
+    pygame.display.flip()
+
+pygame.quit()
+quit() # code end 
+
+# references:
+while not player_found_key:
+    steps += 1
+    print()
+    print('Możesz udać się w kierunkach określonych jako [w/a/s/d')
+
+    move = input('Dokąd idziesz? ')
+    match move.lower():
+        case 'w':
+            player_y += 1
+            if player_y > GAME_HEIGHT:
+                print("Auć! uderzasz w ścianę!")
+                player_y = GAME_HEIGHT
+        case 's':
+            player_y -= 1
+            if player_y < 0:
+                print("Auć! uderzasz w ścianę!")
+                player_y = 0
+        case 'a':
+            player_x -= 1
+            if player_x < 0:
+                print("Auć! uderzasz w ścianę!")
+                player_x = 0
+        case 'd':
+            player_x += 1
+            if player_x > GAME_WIDTH:
+                print("Auć! uderzasz w ścianę!")
+                player_x = GAME_WIDTH
+        case 'q':
+            print('Koniec gry!')
+            quit()
+        case '_':
+            print('Nie wiem dokąd idziesz...')
+            continue
+    
+    if player_x == key_x and player_y == key_y:
+        print('Klucz jest Twój, możesz iść otworzyć skarb')
+        print(f'Wykonałeś/wykonałeś {steps} ruchów.')
+        quit()
+    distance_after_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
+
+    print('before', distance_before_move)
+    print('after', distance_after_move)
+
+    if distance_before_move > distance_after_move:
+        print('Ciepło')
+    else:
+        print('Zimno')
+    distance_before_move = distance_after_move
---
+++ variables.py
@@ -0,0 +1,28 @@
+from random import randint
+from math import sqrt
+import pygame
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+game_name = "Hunt the thimble"
+GAME_WIDTH = 10
+GAME_HEIGHT = 10
+margin_left = 600
+margin_top = 100
+player_color = "#8f00ff"
+key_x = randint(1, GAME_WIDTH)
+key_y = randint(1, GAME_HEIGHT)
+player_x = randint(1, GAME_WIDTH)
+player_y= randint(1, GAME_HEIGHT)
+
+if player_x == key_x and player_y == key_y:
+    key_x = randint(1, GAME_WIDTH)
+    key_y = randint(1, GAME_HEIGHT)
+    player_x = randint(1, GAME_WIDTH)
+    player_y= randint(1, GAME_HEIGHT)
+
+player_found_key = False
+background_color = '#FAF9F6'
+steps = 0
+# obliczam odległość miedzy graczem a skarbem
+distance_before_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
